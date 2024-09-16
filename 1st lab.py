18e743bc6c1ee420998421a26d667c5429dc407e
xraisik <xraisik@gmail.com>
2024-09-16 18:48:15 +0300

    1st lab

---
+++ 1st lab.py
@@ -0,0 +1,21 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(dollars: str) -> float:
+    dollars = dollars.replace("$", "")
+    dollars_float = float(dollars)
+
+    return dollars_float
+
+def percent_to_float(percent: str) -> float:
+    percent = percent.replace("%", "")
+    percent = float(percent) / 100.
+
+    return percent
+
+if __name__ == "__main__":
+    main()
