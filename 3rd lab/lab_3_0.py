6ec420eaf29886ef786b6e1e39abbdee6fa9d543
xraisik <xraisik@gmail.com>
2024-10-14 20:03:35 +0300

    added return list_

--- 3rd lab/lab_3_0.py
+++ 3rd lab/lab_3_0.py
@@ -1,13 +1,15 @@
-def reverse_exercise(list_: list[int]) -> None:
+def reverse_exercise(list_: list[int]) -> list[int]:
     # йдемо тільки до середини списку
     for i in range(len(list_) // 2):
         # міняємо перший і останній елемент місцями
         list_[i], list_[-i-1] = list_[-i-1], list_[i]
 
+    return list_
+
 list_ = [1, 5, 10, 2]
 print("Початковий список:", list_)
-reverse_exercise(list_)
-print("Зміненний список:",list_)
+
+print("Зміненний список:",reverse_exercise(list_))
 
 # перевіряємо ассертом
 assert list_ == [2, 10, 5, 1]
